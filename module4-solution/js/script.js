16ef021db3af8e1485de42ffd5511160a77eb7f2
bine987 <bine987@gmail.com>
2016-03-10 23:06:21 +0100

    tst

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -92,18 +92,16 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-  console.log('a');
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-console.log('b');
+
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
        var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      console.log('c');
-       console.log(chosenCategoryShortName);
+
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -115,17 +113,13 @@ console.log('b');
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       // 
-      console.log(homeHtml);
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
-      console.log('NOVO');
-      console.log(homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that. 
       // ....
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-      console.log('kraj');
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
@@ -153,7 +147,6 @@ dc.loadMenuCategories = function () {
 // Load the menu items view
 // 'categoryShort' is a short_name for a category
 dc.loadMenuItems = function (categoryShort) {
-  console.log("evo ga: " + categoryShort);
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     menuItemsUrl + categoryShort,
